77b630c5188d8074e6413d61559955eecaa84fd5
Alvaro Valdivia <avaldivia.softsytech@gmail.com>
2022-11-10 16:39:19 -0400

    deliverytracker primeras pruebas

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import newman from 'newman'; // require Newman in your project
 
 // call newman.run to pass `options` object and wait for callback
 newman.run({
-    collection: require('./collections/integracionLealtad/IntLealtad.postman_collection.json'),
-    environment: require('./collections/integracionLealtad/ProLealtad.postman_environment.json'),
-    folder: 'QWS',
-    reporters: 'junit' || 'htmlextra',
+    collection: require('./collections/deliveryTracker/DeliveryTrackerApi.postman_collection.json'),
+    environment: require('./collections/deliveryTracker/DeliveryTracker.postman_environment.json'),
+    folder: 'Integration',
+    reporters: 'htmlextra',
 }, function (err) {
     if (err) { throw err; }
     console.log('collection run complete!');
